93c236e8a6654f5fef1340580826d02f2fed9aad
David Christ <davidckss@proton.me>
2024-09-14 19:43:03 +0900

    (chore) add jsdoc and html attributes

--- src/components/common/IconButton.tsx
+++ src/components/common/IconButton.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { IconType } from "react-icons";
 
+/**
+ * Props for the IconButton component.
+ *
+ * @typedef {Object} IconButtonProps
+ * @property {IconType} [icon] - The icon component to be rendered inside the button.
+ * @property {number} [iconSize=24] - Size of the icon.
+ * @property {string} [iconColor] - Color of the icon.
+ * @property {number} [rotation=0] - Rotation angle (in degrees) for the icon.
+ * @property {number} [radius=8] - Border radius of the button.
+ * @property {string} [label] - Optional text label displayed below the icon.
+ * @property {string} [backgroundColor] - Background color of the button.
+ * @property {Function} [onClick] - Function to be called when the button is clicked.
+ */
 export type IconButtonProps = {
   icon?: IconType;
   iconSize?: number;
@@ -10,8 +23,23 @@ export type IconButtonProps = {
   label?: string;
   backgroundColor?: string;
   onClick?: () => void;
-};
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
+/**
+ * IconButton component renders a button with an optional icon and label.
+ *
+ * @param {IconButtonProps} props - Properties passed to configure the button.
+ * @param {IconType} [props.icon] - The icon component to be rendered inside the button.
+ * @param {number} [props.iconSize=24] - Size of the icon.
+ * @param {string} [props.iconColor] - Color of the icon.
+ * @param {number} [props.rotation=0] - Rotation angle (in degrees) for the icon.
+ * @param {number} [props.radius=8] - Border radius of the button.
+ * @param {string} [props.label] - Optional text label displayed below the icon.
+ * @param {string} [props.backgroundColor] - Background color of the button.
+ * @param {Function} [props.onClick] - Function to be called when the button is clicked.
+ *
+ * @returns {JSX.Element} The rendered IconButton component.
+ */
 export const IconButton: React.FC<IconButtonProps> = ({
   icon: Icon,
   iconSize = 24,
@@ -21,11 +49,14 @@ export const IconButton: React.FC<IconButtonProps> = ({
   label,
   backgroundColor,
   onClick,
+  ...rest
 }) => {
   return (
     <button
+      {...rest}
       onClick={onClick}
       style={{
+        ...rest.style,
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
