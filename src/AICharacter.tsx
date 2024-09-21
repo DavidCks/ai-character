2bfb34ce97cc7ef13e7aa586671e944700d9b8c8
David Christ <davidckss@proton.me>
2024-09-22 00:42:38 +0900

    add more pleasant character entry

--- src/AICharacter.tsx
+++ src/AICharacter.tsx
@@ -3,26 +3,65 @@ import { Vector3 } from "three";
 import { Html } from "@react-three/drei";
 import React from "react";
 import { AICharacterManager } from "./AICharacterManager";
-import { useFrame } from "@react-three/fiber";
 import { Controls } from "./components/Controls";
 import { VoiceNames } from "./repo/voices";
+import { emotionAnimations } from "./repo/animations/emotions";
 
 export type AICharacterProps = {
   vrmUrl?: string;
   voiceName?: VoiceNames;
+  onLoad?: (manager: AICharacterManager) => void;
+  onProgress?: (progress: number) => void;
+  showControls?: boolean;
 };
 
 export const AICharacter: React.FC<AICharacterProps> = (props) => {
   const managerRef = React.useRef<AICharacterManager | null>(null);
-  const [progress, setProgress] = React.useState(0);
-  const [showControls, setShowControls] = React.useState(false);
-  const handleLoad = (vrmManager: VRMManager) => {
+  const [showControls, setShowControls] = React.useState(
+    props.showControls ?? false
+  );
+  const handleLoad = React.useCallback(async (vrmManager: VRMManager) => {
+    vrmManager.vrm.scene.visible = false;
+    managerRef.current = new AICharacterManager(vrmManager, props.voiceName);
+
     vrmManager.focusManager.focus({
-      cameraOffset: new Vector3(0, 0, -0.3),
+      cameraOffset: new Vector3(0, 0.0, -0.3),
+      lookAtOffset: new Vector3(0, 0.1, 0),
+      focusIntensity: 1,
     });
-    managerRef.current = new AICharacterManager(vrmManager);
-    setShowControls(true);
-  };
+    const chainPromise = managerRef.current.chainManager.prepareChain({
+      motionAnimationName: "Surprise Wave Greeting",
+      emotion: "joy",
+      intensity: 1,
+      next: {
+        motionAnimationName: "Slight Turn Idle",
+        emotion: "joy",
+        intensity: 0.1,
+        next: {
+          motionAnimationName: "Surprise Wave Greeting",
+          emotion: "joy",
+          intensity: 1,
+          next: null,
+        },
+      },
+    });
+    const chain = await chainPromise;
+    vrmManager.focusManager.focus({
+      cameraOffset: new Vector3(0, 0.0, -0.2),
+      focusIntensity: 0.004,
+    });
+    if (props.onLoad) {
+      await props.onLoad(managerRef.current);
+    }
+    await managerRef.current.chainManager.playPreparedChain(chain);
+    vrmManager.vrm.scene.visible = true;
+  }, []);
+
+  const handleProgress = React.useCallback((progress: number) => {
+    if (props.onProgress) {
+      props.onProgress(progress);
+    }
+  }, []);
 
   return (
     <>
@@ -39,7 +78,7 @@ export const AICharacter: React.FC<AICharacterProps> = (props) => {
           handleLoad(vrmManager);
         }}
         onProgress={(progress: number) => {
-          // setProgress(progress);
+          handleProgress(progress);
         }}
         vrmUrl={props.vrmUrl ?? "aic-runtime-deps/models/Yui.vrm"}
       ></VRMAvatar>
